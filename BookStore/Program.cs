082c581f3fca6ff28cc4130ff89144ff3bf38fa6
Sourab Garg <112079423+SourabGarg@users.noreply.github.com>
2026-05-08 23:29:58 +0530

    Add DbContext configuration for SQL Server

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,3 +1,5 @@
+using BookStore.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore
 {
@@ -13,6 +15,9 @@ namespace BookStore
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<BookContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
