074c0af6ce72688609f81c1d2d28d00994188188
SourabGarg <sharmasourab502@gmail.com>
2026-05-08 12:06:15 +0530

    Initial commit

---
+++ BookStore/Models/Purchaselog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStore.Models;
+
+public partial class Purchaselog
+{
+    public int UserId { get; set; }
+
+    public int InventoryId { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
