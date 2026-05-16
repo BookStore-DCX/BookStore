770016757764aacb34aa385f2db71e94c029b0ec
Raghvi Sharma <raghvi29sharma@gmail.com>
2026-05-16 11:37:14 +0530

    Added mvc

---
+++ BookStores.MVC/Controllers/AuthorController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStores.MVC.Controllers
+{
+    public class AuthorController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
